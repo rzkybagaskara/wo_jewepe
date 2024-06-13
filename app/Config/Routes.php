286dd2a33259f5dfe31fbd3bdc38c03387960677
Rizky Bagaskara <rizky.6abu@hotmail.com>
2024-06-13 20:35:15 +0700

    add routes for home, login, and wedding packet

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/loginAdmin', 'Home::loginAdmin');
+$routes->get('/daftarPaket', 'Home::daftarPaket');
+$routes->get('/tambahPaket', 'Home::tambahPaket');
+$routes->get('/about', 'Home::about');
