546ee86c71f7378e10f9aa901d8f8ba516c7dbee
Rizky Bagaskara <rizky.6abu@hotmail.com>
2024-06-14 23:15:24 +0700

    add routes for Daftar Pesanan and Order Form Pesanan

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,11 +8,15 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 //$routes->get('/about', 'Home::about');
 $routes->get('/rsvp', 'User::rsvp');
-$routes->get('/loginAdmin', 'Home::loginAdmin');
-//$routes->post('login', 'Login::do_login');
+$routes->post('/user/tambahPesanan', 'User::tambahPesanan');
+$routes->post('/updatePesanan/(:any)', 'Home::updatePesanan/$1');
+$routes->post('/deletePesanan/(:any)', 'Home::deletePesanan/$1');
 $routes->get('/daftarPaket', 'Home::daftarPaket');
 $routes->get('/tambahPaket', 'Home::tambahPaket');
 $routes->post('/deletePaket/(:any)', 'Home::deletePaket/$1');
 $routes->post('/updatePaket/(:any)', 'Home::updatePaket/$1');
 $routes->post('/tambahPaket', 'Home::tambahPaket');
 $routes->get('/daftarPesanan', 'Home::daftarPesanan');
+
+//$routes->get('/loginAdmin', 'Home::loginAdmin');
+//$routes->post('login', 'Login::do_login');
