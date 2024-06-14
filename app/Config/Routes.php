eca4ffc9727e298118fbdbb071a978050e14957c
Rizky Bagaskara <rizky.6abu@hotmail.com>
2024-06-14 16:42:58 +0700

    add routes for RSVP (Order Form) for Paket Wedding

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,8 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+//$routes->get('/about', 'Home::about');
+$routes->get('/rsvp', 'User::rsvp');
 $routes->get('/loginAdmin', 'Home::loginAdmin');
 //$routes->post('login', 'Login::do_login');
 $routes->get('/daftarPaket', 'Home::daftarPaket');
@@ -13,4 +15,4 @@ $routes->get('/tambahPaket', 'Home::tambahPaket');
 $routes->post('/deletePaket/(:any)', 'Home::deletePaket/$1');
 $routes->post('/updatePaket/(:any)', 'Home::updatePaket/$1');
 $routes->post('/tambahPaket', 'Home::tambahPaket');
-$routes->get('/about', 'Home::about');
+$routes->get('/daftarPesanan', 'Home::daftarPesanan');
